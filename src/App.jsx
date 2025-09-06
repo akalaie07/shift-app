7ca9a654b98a6606188d7413eb86d9b1c541fb7b
akalaie07 <akalaie@icloud.com>
2025-09-06 11:18:24 +0200

    Update 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function NewShiftForm({ onCreate }) {
       </div>
 
       <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition">
-        Schicht starten
+        Schicht speichern
       </button>
     </form>
   );
